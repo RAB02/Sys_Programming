834d9f83b3084e041bdd4e94a966963197ddfba1
Ramon Bernal <81490639+RAB02@users.noreply.github.com>
2024-12-17 12:21:37 -0600

    Delete test_hwrk directory

--- test_hwrk/target/debug/deps/test_hwrk-09d30375ad2ad443.d
+++
@@ -1,6 +0,0 @@
-/workspaces/Sys_Programming/test_hwrk/target/debug/deps/test_hwrk-09d30375ad2ad443: src/main.rs src/bank_account.rs
-
-/workspaces/Sys_Programming/test_hwrk/target/debug/deps/test_hwrk-09d30375ad2ad443.d: src/main.rs src/bank_account.rs
-
-src/main.rs:
-src/bank_account.rs:
--- test_hwrk/target/debug/test_hwrk.d
+++
@@ -1,1 +0,0 @@
-/workspaces/Sys_Programming/test_hwrk/target/debug/test_hwrk: /workspaces/Sys_Programming/test_hwrk/src/bank_account.rs /workspaces/Sys_Programming/test_hwrk/src/main.rs
